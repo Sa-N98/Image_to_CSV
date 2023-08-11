6818f6ab20988c27b9694fa9ae1b4e3f71ef58f1
Saranya Nayak <san00kam1@proton.me>
2023-08-11 18:54:12 +0530

    made modifications to docTr.py

--- docTR.py
+++ docTR.py
@@ -7,60 +7,62 @@ Usage : Converts the docTR output in to list of word and its location coordinate
         NOTE: Import function docTr from the module.
 """
 
+
+
+
+
 from PIL import Image
 import torch
 import torchvision
 from torchvision.io import read_image
 from torchvision.utils import draw_bounding_boxes
-from doctr.io import DocumentFile
-from doctr.models import ocr_predictor
+# from doctr.io import DocumentFile
+# from doctr.models import ocr_predictor
+import requests
+import json
 
 
-model = ocr_predictor(det_arch='db_resnet50', reco_arch='crnn_vgg16_bn', pretrained=True)
-                     
+# model = ocr_predictor(det_arch='db_resnet50', reco_arch='crnn_vgg16_bn', pretrained=True)
+
+def is_close(value1, value2, threshold=3):
+    return abs(value1 - value2) <= threshold
+                
 def OCR(Image_path:str)->dict:
-    """Read an image from a path to the OCR
-
-    Args:
-        Image_path (str): path to an image file
-
-    Returns:
-        Dictionary : Dictionary containing info regarding words in the image
+    """I have no Idea how this works it just works
     """
     
-    single_img_doc = DocumentFile.from_images(Image_path)
-    result = model(single_img_doc)
-    dict_output = result.export()
-    return dict_output
+    headers = {
+                    'accept': 'application/json',
+                    # requests won't add a boundary if this header is set when you pass files=
+                    # 'Content-Type': 'multipart/form-data',
+                }
 
-def JSON_Processor(Json:dict)->list:
-    """Extracts the words and there coordinates and stores then in two separate list for docTR dictionary.
-       NOTE: The list  of words and coordinates are mapped 
-       i.e word in the first index of the label list maps to the coordinates in the first index of the bbox_coordinates list
+    files = {
+        'file': open(Image_path, 'rb'),
+    }
 
-    Args:
-        Json (dict): DocTR output dictionary.
+    response = requests.post('http://216.48.191.150:8005/ocr_invoice_image', headers=headers, files=files)
+    res = json.loads(response.text)['ocr_result']
+    res1 = json.loads(res)
+    data=list()
 
-    Returns:
-        lables : list containing words in the image.
-        bbox_coordinates : List of coordinates of the words in the image.
-    """
+    for entry in res1:
+        data.append([entry['value'],entry['box']])
+    
+    grouped_data = []
+    for entry in data:
+        found_group = False
 
-    word_coordinate_pairs=list()
-    image_height,image_width =Json['pages'][0]['dimensions']
+        for group in grouped_data:
+            if is_close(group[0][1][1], entry[1][1], threshold=13):  # Adjust threshold as needed
+                group.append(entry)
+                found_group = True
+                break
 
-    for pages in Json['pages']:
-        for blocks in pages['blocks']:
-            temp=list()
-            for word in blocks['lines'][0]['words']:
-                temp.append([word ['value'],
-                    [word ['geometry'][0][0] * image_width,
-                    word ['geometry'][0][1] * image_height,
-                    word ['geometry'][1][0] * image_width,
-                    word ['geometry'][1][1] * image_height]])
-            word_coordinate_pairs.append(temp)
-    # further processing to pair words of same sentance or cell.
-    for items in word_coordinate_pairs:
+        if not found_group:
+            grouped_data.append([entry])
+    
+    for items in grouped_data:
         if len(items)>1:
             for i in range(len(items) - 1):
                 first_entry = items[i]
@@ -69,19 +71,70 @@ def JSON_Processor(Json:dict)->list:
                     items[i]='remove'
                     items[i + 1]=[first_entry[0]+' '+second_entry[0], [first_entry[1][0],first_entry[1][1],second_entry[1][2],second_entry[1][3]]]         
 
-    for items in word_coordinate_pairs:
+    for items in grouped_data:
         if len(items)>1:
             while 'remove' in items:
                 items.remove('remove')
-
+    
     lables=list()
     bbox_coordinates=list() 
 
-    for items in word_coordinate_pairs:
+    for items in grouped_data:
         for item in  items:
             lables.append(item[0])
             bbox_coordinates.append(item[1])
+
     return lables,bbox_coordinates
+
+# def JSON_Processor(Json:dict)->list:
+#     """Extracts the words and there coordinates and stores then in two separate list for docTR dictionary.
+#        NOTE: The list  of words and coordinates are mapped 
+#        i.e word in the first index of the label list maps to the coordinates in the first index of the bbox_coordinates list
+
+#     Args:
+#         Json (dict): DocTR output dictionary.
+
+#     Returns:
+#         lables : list containing words in the image.
+#         bbox_coordinates : List of coordinates of the words in the image.
+#     """
+
+#     word_coordinate_pairs=list()
+#     image_height,image_width =Json['pages'][0]['dimensions']
+
+#     for pages in Json['pages']:
+#         for blocks in pages['blocks']:
+#             temp=list()
+#             for word in blocks['lines'][0]['words']:
+#                 temp.append([word ['value'],
+#                     [word ['geometry'][0][0] * image_width,
+#                     word ['geometry'][0][1] * image_height,
+#                     word ['geometry'][1][0] * image_width,
+#                     word ['geometry'][1][1] * image_height]])
+#             word_coordinate_pairs.append(temp)
+#     # further processing to pair words of same sentance or cell.
+#     for items in word_coordinate_pairs:
+#         if len(items)>1:
+#             for i in range(len(items) - 1):
+#                 first_entry = items[i]
+#                 second_entry = items[i + 1]
+#                 if second_entry[1][0] - first_entry[1][2]<3:
+#                     items[i]='remove'
+#                     items[i + 1]=[first_entry[0]+' '+second_entry[0], [first_entry[1][0],first_entry[1][1],second_entry[1][2],second_entry[1][3]]]         
+
+#     for items in word_coordinate_pairs:
+#         if len(items)>1:
+#             while 'remove' in items:
+#                 items.remove('remove')
+
+#     lables=list()
+#     bbox_coordinates=list() 
+
+#     for items in word_coordinate_pairs:
+#         for item in  items:
+#             lables.append(item[0])
+#             bbox_coordinates.append(item[1])
+#     return lables,bbox_coordinates
 
 def visualizer(Image_path:str,bbox_coordinates:list,lables:list):
     """Read an image from a path torchvision for drawing border boxes.
@@ -113,7 +166,7 @@ def docTr(Image_path:str ,visualize=False)->list:
        EXAMPLE: [ .... ['Ocean Freight', [0.0, 136.013671875, 137.314453125, 159.498046875]] ....]
                 
     """
-    lables,bbox_coordinates=JSON_Processor(OCR(Image_path))
+    lables,bbox_coordinates=OCR(Image_path)
     if visualize:
         visualizer(Image_path=Image_path,bbox_coordinates=bbox_coordinates,lables=lables)
     return [ [word, coordinates] for word ,coordinates in zip(lables, bbox_coordinates)]
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from docTR import docTr
 from make_CSV import make_csv
 
 
-path='TEST/a80bc3bc2a9fb28faba44cf6f64ce8e58cafb8d1575f2557e7eb497e.jpg'
-word_coordinates=docTr(ip.padder(path,100,100))
+path='/Users/901002/work/Invoice/Samples/8dff599aa668fe37a757420b23d28b6706ed7ed06558017eb8192e78.jpg'
+word_coordinates=docTr(ip.padder(path,100,100),True)
 
-make_csv(data=word_coordinates, headers_list=['Description', 'Qty', 'Basis', 'Rate', 'Charge'])
+make_csv(data=word_coordinates, headers_list=['Description', 'Quantity', 'Quantité', 'Code Code', 'Weight Poids', 'Rate', 'Discount%', 'Gross Amount', 'Net Amount'])
