79e9212c9810dcbb0a0c82f1c666e06613ac70e0
Saranya Nayak <san00kam1@proton.me>
2023-08-18 09:40:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ from make_CSV import make_csv
 
 
 path='/Users/901002/work/Invoice/Samples/45436b1fb879160aea90ce6e3dd21e7da087a465954b302ecb69567d.jpg'
-word_coordinates=docTr(ip.padder(path,1000,800),True)
+word_coordinates=docTr(ip.padder(path),True)
 
 make_csv(data=word_coordinates, headers_list=['Description', 'Type', 'Per Unit', 'Units', 'FxRate', 'Total'])
